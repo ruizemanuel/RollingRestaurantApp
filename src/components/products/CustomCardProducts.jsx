6d86d986118ae0ad3964569339a19daaabde1b96
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-11-22 22:46:44 -0300

    little fix

--- src/components/products/CustomCardProducts.jsx
+++ src/components/products/CustomCardProducts.jsx
@@ -25,7 +25,6 @@ export const CustomCardProducts = ({ itemData }) => {
 
   const [favorite, setFavorite] = useState(checkFavoriteStatus());
 
-
   const onPressFavorite = async (id, item) => {
     const email = await AsyncStorage.getItem('email');
     let userFavorites = await AsyncStorage.getItem('userFavorites');
